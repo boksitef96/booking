563b8bd0668d285011ab7e379066f89d911de5b4
miljandenic96 <miljan1996denic@gmail.com>
2018-12-20 22:02:34 +0100

    Weather service connected

--- Code/Booking/Controllers/ExampleController.cs
+++ Code/Booking/Controllers/ExampleController.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
 using Booking.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net;
 
 namespace Booking.Controllers
 {
@@ -22,13 +25,27 @@ namespace Booking.Controllers
             _context.Dispose();
         }
 
-        
+
         public ActionResult ShowExamples()
         {
-            var examples = _context.Exapmles.ToList();
-            var model = new List<Example>();
-            model = examples;
-            return View(model);
+            using (var client = new WebClient())
+            {
+                string createUrl = "https://api.openweathermap.org/data/2.5/weather?q=Nis,srb&APPID=8aa8eb55555024179f696bdbe89a97b4&";
+                // client.Headers.Add("IH-Authorization", ihAuth);
+                var url = new Uri(createUrl);
+                try
+                {
+                    var response = client.DownloadString(url);
+                }
+                catch (Exception e)
+                {
+                    string error = e.Message;
+                }
+                var examples = _context.Exapmles.ToList();
+                var model = new List<Example>();
+                model = examples;
+                return View(model);
+            }
         }
     }
 }
